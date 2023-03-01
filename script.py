505b85262e163c9d41ed8e7b1d2729e3182aab7d
Vignesh Pb <plumbum_7@yahoo.com>
2023-03-01 14:30:58 +0530

    Add code to send slack notifications in case of a new post

--- script.py
+++ script.py
@@ -10,6 +10,8 @@ def update_file(file, subreddit, title, selftext):
 
 username = '<username>'
 url = "https://www.reddit.com/user/" + username + "/submitted.json"
+slack_token = '<slack_token>'
+slack_channel = '<slack_channel>'
 headers = {
     'Accept': 'application/json',
     'User-agent': 'Track Script'
@@ -26,8 +28,8 @@ for post in posts:
     break
 
 subreddit = top_post['data']['subreddit']
-title = top_post['data']['title'].replace('\r', '').replace('\n', '')
-selftext = top_post['data']['selftext'].replace('\r', '').replace('\n', '')
+title = top_post['data']['title'].strip().replace('\r', '').replace('\n', '')
+selftext = top_post['data']['selftext'].strip().replace('\r', '').replace('\n', '')
 
 with open(file_name, 'r') as file:
     contents = file.readlines()
@@ -39,3 +41,13 @@ with open(file_name, 'r') as file:
         previous_selftext = contents[2].strip().replace('\r', '').replace('\n', '')
         if previous_subreddit != subreddit or previous_title != title or previous_selftext != selftext:
             update_file(file, subreddit, title, selftext)
+            slack_url = 'https://slack.com/api/chat.postMessage'
+            message_body = {
+                'channel': slack_channel,
+                'text': 'A new post has been made *' + title + '* in the subreddit *' + subreddit + '* with the body \n>' + selftext + ''
+            }
+            headers = {
+                'Authorization': 'Bearer ' + slack_token,
+                'Content-type': 'application/json; charset=utf-8'
+            }
+            response = requests.post(slack_url, json = message_body, headers = headers)
